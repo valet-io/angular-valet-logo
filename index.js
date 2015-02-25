802654a06efe81f9b168f2de45df616c67e50213
Ben Drucker <bvdrucker@gmail.com>
2015-02-25 12:17:42 -0500

    Add styles

--- index.js
+++ index.js
@@ -2,12 +2,16 @@
 
 import angular from 'angular';
 import logo from './logo.svg';
+import styles from './styles.css';
+import insertStyles from 'insert-css';
 
 const directive = {
   restrict: 'E',
   template: logo,
   templateNamespace: 'svg'
 };
+
+insertStyles(styles);
 
 export default angular.module('valet-io.logo', [])
   .directive('valetIoLogo', function () {
